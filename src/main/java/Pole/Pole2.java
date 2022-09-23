dbbc28c924663e2d84027250222605308b3cdd97
felippe97 <84409435+felippe97@users.noreply.github.com>
2022-09-23 16:49:57 +0200

    .

--- src/main/java/Pole/Pole2.java
+++ src/main/java/Pole/Pole2.java
@@ -11,10 +11,12 @@ public class Pole2 {
 	public static void main(String[] args) {
 
 		int[] num = new int[10];
+		
 		int del;
 		for (int i = 0; i < num.length; i++) {
 			num[i] = i;
 		}
+		System.out.println("Zakladne pole " + Arrays.toString(num));
 		Random rand = new Random();
 		for (int i = 0; i < num.length; i++) {
 			int r = rand.nextInt(num.length);
@@ -23,17 +25,23 @@ public class Pole2 {
 			num[i] = temp;
 			
 		}
-		System.out.println(Arrays.toString(num));
+		System.out.println("Prehadzane pole " + Arrays.toString(num));
 		
-		del =  num.length - 1;
-		System.out.println(del);
 		
-		int max = 10;
+		
+		int max = 9;
 		int min = 0;
 
 		int random = (int) (Math.random() * (max - min + 1) + min);
-
-		System.out.println("nahodne cislo vybrane" + random + "\n");
+		int[] newNum = new int[num.length -1];
+		for(int i=0, k=0;i<num.length;i++){
+            if(i!=random){
+                newNum[k]=num[i];
+                k++;
+            }
+        }
+		System.out.println("Odstraneny index random " + Arrays.toString(newNum));
+		System.out.println("nahodny index  " + random + "\n");
 
 		boolean found = false;
 		for (int n : num) {
